bfc8796330fa0b39cff845e4172376bf098d5063
albertgoncalves <goncalves.albert@gmail.com>
2019-05-25 18:41:28 -0400

    stole data from <hoquei>

--- example.jl
+++ example.jl
@@ -2,9 +2,23 @@
 
 using CSV, DataFrames
 
+function summary(df)
+    for f in [names, describe]
+        println(f(df))
+    end
+end
+
+function readcsv(filename)
+    CSV.read(filename, delim=";")
+end
+
 function main()
-    df = DataFrame(A = 1:4, B = ["M", "F", "F", "M"])
-    println(df)
+    events_csv = "data/events.csv"
+    shifts_csv = "data/shifts.csv"
+    if all(isfile, [events_csv, shifts_csv])
+        events = readcsv(events_csv)
+        summary(events)
+    end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
